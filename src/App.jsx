01e943c702ce9504c98057af1d3758f277d2b29f
Arief <mrarief0110@gmail.com>
2023-12-06 12:47:55 +0530

    Props-updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@ import Card from "./Component/Card";
 import "./App.css";
 
 function App() {
-  let foodItesms = [
-    {
-      name: "burger",
-    },
-    {
-      name: "pizza",
-    },
-  ];
+  let foodItesms = [{
+    name: "Spaghetti Bolognese",
+    description: "Classic Italian pasta with rich meat sauce",
+    price: 12.99,
+  },
+  {
+    name: "Chicken Caesar Salad",
+    description: "Fresh romaine lettuce, grilled chicken, and Caesar dressing",
+    price: 9.99,
+  },
+  {
+    name: "Margherita Pizza",
+    description: "Traditional pizza with tomato, mozzarella, and basil",
+    price: 14.99,
+  },
+  {
+    name: "Sushi Platter",
+    description: "Assorted sushi rolls with soy sauce and wasabi",
+    price: 18.99,
+  },
+  {
+    name: "Chocolate Fondue",
+    description: "Melted chocolate served with fruits and marshmallows",
+    price: 8.99,
+  },
+];
   const country = "India";
   return (
     <div className="flex">
